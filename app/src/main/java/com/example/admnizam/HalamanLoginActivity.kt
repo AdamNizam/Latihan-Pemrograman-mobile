131520b400c382e604faca5a44a35b56486801b4
martina fariza <martinafariza08@gmail.com>
2023-11-03 11:39:04 -0700

    Intent

---
+++ app/src/main/java/com/example/admnizam/HalamanLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.admnizam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_login)
+    }
+}
