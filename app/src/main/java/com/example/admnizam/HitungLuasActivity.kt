388c9b0e39de2674051cf27a86b128be047c42d0
martina fariza <martinafariza08@gmail.com>
2023-11-03 02:11:32 -0700

    view Binding

--- app/src/main/java/com/example/admnizam/HitungLuasActivity.kt
+++ app/src/main/java/com/example/admnizam/HitungLuasActivity.kt
@@ -4,42 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.example.admnizam.databinding.ActivityHitungLuasBinding
 import com.google.android.material.textfield.TextInputEditText
 
 class HitungLuasActivity : AppCompatActivity() {
 
-    private lateinit var lebarEditText: TextInputEditText
-    private lateinit var panjangEditText: TextInputEditText
-    private lateinit var TinggiEditText: TextInputEditText
-    private lateinit var hitungButton: Button
-    private lateinit var hasilTextView: TextView
+    private lateinit var binding : ActivityHitungLuasBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hitung_luas)
+        binding = ActivityHitungLuasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.hitungButton.setOnClickListener {
+            val lebar = binding.lebarEditText.text.toString()
+            val panjang = binding.panjangEditText.text.toString()
+            val tinggi = binding.tinggiEditText.text.toString()
+            val hasil = panjang.toInt() * lebar.toInt() * tinggi.toInt()
+            binding.hasilTextView.text = hasil.toString() + "m2"
 
-        initKomponent()
-        hitungButton.setOnClickListener {
-            val lebar = lebarEditText.text.toString()
-            val panjang = panjangEditText.text.toString()
-            val tinggi = TinggiEditText.text.toString()
-
-            if (panjang.isEmpty() || lebar.isEmpty() || tinggi.isEmpty()) {
-                hasilTextView.text = "inputan tidak boleh kosong"
-            } else {
-                val hasil = panjang.toInt() * lebar.toInt() * tinggi.toInt()
-                hasilTextView.text = hasil.toString() + " m2"
-            }
         }
 
-    }
-
-    private fun initKomponent() {
-        lebarEditText = findViewById(R.id.lebarEditText)
-        panjangEditText = findViewById(R.id.panjangEditText)
-        TinggiEditText = findViewById(R.id.TinggiEditText)
-        hitungButton = findViewById(R.id.hitungButton)
-        hasilTextView = findViewById(R.id.hasilTextView)
+//        initKomponent()
+//        binding.hitungButton.setOnClickListener {
+//            val lebar = binding.lebarEditText.text.toString()
+//            val panjang = binding.panjangEditText.text.toString()
+//            val tinggi =binding. TinggiEditText.text.toString()
+//
+////            if (panjang.isEmpty() || lebar.isEmpty() || tinggi.isEmpty()) {
+////                hasilTextView.text = "inputan tidak boleh kosong"
+////            } else {
+////                val hasil = panjang.toInt() * lebar.toInt() * tinggi.toInt()
+////                hasilTextView.text = hasil.toString() + " m2"
+////            }
+//        }
 
     }
 }
